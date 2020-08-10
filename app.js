0b5c63674adb4c217335a2cb3881ea2561962366
Lucas Silva <martssilvapro@gmail.com>
2020-08-10 17:47:56 -0300

    funcao de cadastro criada

---
+++ app.js
@@ -0,0 +1,13 @@
+// Capturando as informações dos inputs html
+function cadastrarDespesa() {
+  // Referenciando as informações em variáveis para uso posterior  
+  let ano = document.getElementById('ano')
+  let mes =  document.getElementById('mes')
+  let dia =  document.getElementById('dia')  
+  let tipo =  document.getElementById('tipo')
+  let descricao =  document.getElementById('descricao')
+  let valor = document.getElementById('valor')
+
+  console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+ 
+}
