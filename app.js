59dbd5fcf71dd6c4eb757ab4923e3aa6daffca01
Lucas Silva <martssilvapro@gmail.com>
2020-08-17 19:53:10 -0300

    projeto finalizado

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ class Bd {
             if ( despesa === null ) {
                 continue
             }
-
+            despesa.id = i
             despesas.push(despesa)
         }
 
@@ -120,6 +120,11 @@ class Bd {
 
         return despesasFiltradas
     }
+
+    remover(id) {
+        localStorage.removeItem(id)
+    }
+
 }
 
 let bd = new Bd() 
@@ -216,6 +221,23 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
         linha.insertCell(1).innerHTML = d.tipo
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        // Criar o botão de exclusão
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"><i/>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function() {
+            // Remover despesa
+
+            let id = this.id.replace('id_despesa_', '')
+
+            bd.remover(id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
     }) 
 }   
 
